8d363b366ac16c007e7c1d065c89f9374292d43e
algisju <69957906+algisju@users.noreply.github.com>
2021-05-21 09:58:55 -0400

    Add files via upload

---
+++ venueclasses.py
@@ -0,0 +1,7 @@
+class Venue:
+  def __init__(self, id, name, date, tables, persons):
+    self.id = id
+    self.name = name
+    self.date = date
+    self.tables = tables
+    self.persons = persons
