7bd6c889a339d20063aed6262f4c8ca5023aab37
algisju <69957906+algisju@users.noreply.github.com>
2021-09-05 02:31:09 -0400

    Delete yo.sh

--- yo.sh
+++
@@ -1,7 +0,0 @@
-while read subdomain; do
-if host "$subdomain" > /dev/null; then
-# If host is live, print it into
-# a file called "live.txt".
-echo "$subdomain" >> live.txt
-fi
-done < validsub.txt
